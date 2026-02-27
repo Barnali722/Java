26ce93b4e595499a26d38bd9cac52fe63d0b07ff
Barnali Tanti <tantibarnali520@gmail.com>
2026-02-27 10:44:57 +0530

    Add files via upload

---
+++ Pythagorean_Triples.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Pythagorean_Triples
+{
+    public static void main(String args[])
+    {
+        int i,j,k;
+        for(i=1;i<=30;i++)
+        {
+          for(j=1;j<=30;j++)
+          {
+              for(k=1;k<=30;k++)
+              {
+                  if(i*i+j*j==k*k)
+                  {
+                  System.out.println(i + ", " + j + ", " + k);
+                }
+                }
+            }
+        }
+    }
+}
