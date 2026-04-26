5eafc4ba6337a54bb2272c800004ab08b024788e
Barnali Tanti <tantibarnali520@gmail.com>
2026-04-26 19:53:53 +0530

    Add files via upload

---
+++ street_animal.java
@@ -0,0 +1,31 @@
+public class street_animal
+{
+    void eat()
+    {
+        System.out.println("eating...");
+    }  
+}  
+class Dog extends street_animal
+{  
+    void bark()
+    {
+        System.out.println("barking...");
+    }  
+}  
+class BabyDog extends Dog
+{  
+    void weep()
+    {
+        System.out.println("weeping...");
+    }  
+}  
+class TestInheritance2
+    {  
+    public static void main(String args[])
+    {  
+        BabyDog d=new BabyDog();  
+        d.weep();  
+        d.bark();  
+        d.eat();  
+    }
+} 
